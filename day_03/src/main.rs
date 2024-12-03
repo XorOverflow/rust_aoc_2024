e56500611be43ef91f471a3dc5a21f428203ed19
XorOverflow <152518112+XorOverflow@users.noreply.github.com>
2024-12-03 15:34:00 +0100

    Day 03, part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -27,6 +27,45 @@ fn scan_muls(inputs: &Vec<String>) -> u64 {
     result
 }
 
+fn scan_muls_do_dont(inputs: &Vec<String>) -> u64 {
+    // Add a capture group matching the conditional command.
+    // Use named capture groups to distinguish the different cases.
+    // We must use a single regex and not multiple, to be able to iterate
+    // in order between the conditionals and the muls.
+    let re = Regex::new(r"(?<do>do\(\))|(?<dont>don't\(\))|mul\((?<arg1>[0-9]+),(?<arg2>[0-9]+)\)")
+        .unwrap();
+
+    let mut enabled: bool = true;
+    let mut result: u64 = 0;
+
+    for s in inputs {
+        for cap in re.captures_iter(s) {
+            if let Some(_) = cap.name("do") {
+                enabled = true;
+            } else if let Some(_) = cap.name("dont") {
+                enabled = false;
+            } else if enabled {
+                if let Some(arg1) = cap.name("arg1") {
+                    if let Some(arg2) = cap.name("arg2") {
+                        let arg1 = arg1.as_str();
+                        let arg2 = arg2.as_str();
+                        //eprintln!("parsed enabled mul  {arg1} * {arg2}");
+                        let arg1 = u64::from_str(arg1).unwrap();
+                        let arg2 = u64::from_str(arg2).unwrap();
+                        result += arg1 * arg2;
+                    } else {
+                        panic!("regex matched neither do, dont or arg2");
+                    }
+                } else {
+                    panic!("regex matched neither do, dont or arg1");
+                }
+            }
+        }
+    }
+
+    result
+}
+
 fn main() {
     let mut lines = Vec::<String>::new();
 
@@ -55,4 +94,6 @@ fn main() {
     println!("mul = {mulsum}");
 
     // Part 2
+    let mulsum: u64 = scan_muls_do_dont(&lines);
+    println!("mul do/don't= {mulsum}");
 }
