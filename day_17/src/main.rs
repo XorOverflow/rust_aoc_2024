bc6471172a7854e2a182de8ef1f09a15094ec46e
XorOverflow <152518112+XorOverflow@users.noreply.github.com>
2024-12-18 00:07:11 +0100

    Day 17, part 2
    Solves the problem with a bit of manual tweaking for the
    initial seed.
    Still looking for the error in general algo.

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -42,6 +42,58 @@ impl Machine {
         while self.execute_one_step() {}
     }
 
+    /// Run the program. If the output is different
+    /// than the program code, stops and return false.
+    /// If it halts and output == program, return true.
+    fn run_until_halt_or_non_quine(&mut self) -> bool {
+        self.run_until_halt_or_non_quine_or_outlen(self.program.len() + 1)
+    }
+
+    /// Run the program. If the output is different
+    /// than the program code, stops and return false.
+    /// If it halts, or the output length reached the
+    /// specified size, and output == program, return true.
+    fn run_until_halt_or_non_quine_or_outlen(&mut self, maxlen: usize) -> bool {
+        // Compare only new "out" elements (no need
+        // to compare the full array every time)
+        let mut checked_len = 0;
+        loop {
+            let halted = !self.execute_one_step();
+            let out_len = self.output.len();
+            if out_len > checked_len {
+                if out_len > self.program.len() {
+                    // output longer than program
+                    //println!("output too long");
+                    //self.print_output();
+                    return false;
+                }
+                if self.output[out_len - 1] != self.program[out_len - 1] {
+                    // latest element differs
+                    //println!("output differs at end");
+                    //self.print_output();
+                    return false;
+                }
+                checked_len = out_len;
+            }
+            if out_len == maxlen {
+                return true;
+            }
+            if halted {
+                //self.print_output();
+                return self.program.len() == self.output.len();
+            }
+        }
+    }
+
+    /// Reboots the machine with a specific starting register value.
+    fn reset_with_register(&mut self, a: usize) {
+        self.instruction_ptr = 0;
+        self.register_a = a;
+        self.register_b = 0;
+        self.register_c = 0;
+        self.output.truncate(0);
+    }
+
     /// Run the program for exactly one instruction
     /// (at current IP).
     /// Returns true if it executed,
@@ -55,10 +107,9 @@ impl Machine {
         self.instruction_ptr += 2;
 
         match ins {
-            // Div A by power of 2, multiple register dest
+            // Div A by power of 2 (= bit shift), multiple register dest
             Adv(d) | Bdv(d) | Cdv(d) => {
-                let den = 1 << self.get_combo_value(d);
-                let res = self.register_a / den;
+                let res = self.register_a >> self.get_combo_value(d);
                 match ins {
                     Adv(_) => self.register_a = res,
                     Bdv(_) => self.register_b = res,
@@ -88,8 +139,13 @@ impl Machine {
 
     /// Return the Instruction encoded at current IP
     fn decode_current_instruction(&self) -> Instruction {
-        let i = self.program[self.instruction_ptr];
-        let o = self.program[self.instruction_ptr + 1];
+        self.decode_instruction_at(self.instruction_ptr)
+    }
+
+    /// Return the Instruction encoded at any valid program offset
+    fn decode_instruction_at(&self, p: usize) -> Instruction {
+        let i = self.program[p];
+        let o = self.program[p + 1];
 
         match i {
             0 => Adv(ComboOperand(o)),
@@ -127,6 +183,71 @@ impl Machine {
         println!("Program output:");
         println!("{s}");
     }
+
+    fn get_combo_representation(o: ComboOperand) -> String {
+        match o.0 {
+            0..=3 => o.0.to_string(),
+            4 => String::from("A"),
+            5 => String::from("B"),
+            6 => String::from("C"),
+            _ => String::from("???"),
+        }
+    }
+
+    /// For debug: output readable assembly
+    fn pretty_print_assembly(&self) {
+        for k in (0..self.program.len()).step_by(2) {
+            eprint!("{:02}: ", k);
+            let ins = self.decode_instruction_at(k);
+            match ins {
+                Adv(d) => eprintln!("ADV {}", Self::get_combo_representation(d)),
+                Bdv(d) => eprintln!("BDV {}", Self::get_combo_representation(d)),
+                Cdv(d) => eprintln!("CDV {}", Self::get_combo_representation(d)),
+                Bxl(x) => eprintln!("BXL {}", x.0),
+                Bst(v) => eprintln!("BST {}", Self::get_combo_representation(v)),
+                Jnz(p) => eprintln!("JNZ {}", p.0),
+                // Xor C into B
+                Bxc => eprintln!("BXC"),
+                // out
+                Out(o) => eprintln!("OUT {} % 8", Self::get_combo_representation(o)),
+            }
+        }
+    }
+
+    fn pretty_print_pseudocode(&self) {
+        for k in (0..self.program.len()).step_by(2) {
+            eprint!("{:02}: ", k);
+            let ins = self.decode_instruction_at(k);
+            match ins {
+                Adv(d) => eprintln!("A = A >> {}", Self::get_combo_representation(d)),
+                Bdv(d) => eprintln!("B = A >> {}", Self::get_combo_representation(d)),
+                Cdv(d) => eprintln!("C = A >> {}", Self::get_combo_representation(d)),
+                Bxl(x) => eprintln!("B = B xor {}", x.0),
+                Bst(v) => eprintln!("B = {} % 8", Self::get_combo_representation(v)),
+                Jnz(p) => eprintln!("If A != 0 JMP {}", p.0),
+                Bxc => eprintln!("B = B xor C"),
+                // out
+                Out(o) => eprintln!("OUT {} % 8", Self::get_combo_representation(o)),
+            }
+        }
+    }
+}
+
+fn brute_force(machine: &mut Machine) {
+    let lower_a = 1 << (machine.program.len() - 1) * 3;
+    let higher_a = 2 << (machine.program.len() - 1) * 3;
+    println!("Range : {lower_a} .. {higher_a}");
+    for k in lower_a..higher_a {
+        if k % 100000 == 0 {
+            eprintln!("testing {k}...");
+        }
+        machine.reset_with_register(k);
+        if machine.run_until_halt_or_non_quine() {
+            machine.print_output();
+            println!("Part2 : Register A value for Quine = {k}");
+            break;
+        }
+    }
 }
 
 fn main() {
@@ -160,7 +281,156 @@ fn main() {
             .collect();
     }
 
+    println!("============");
+    machine.pretty_print_assembly();
+    println!("============");
+    machine.pretty_print_pseudocode();
+    println!("============");
+
     machine.run_until_halt();
     println!("Part1:");
     machine.print_output();
+
+    // Brute_forcing didn't find after 2151200000 iterations.
+    // reading the assembly output, my input is a loop
+    // making some operations and dividing A by 2^3 (8), seems
+    // to be a shifting of a long int 3 bits by 3 bits on each loop.
+    // Program halts by a final JNZ whenever A is 0 at this point.
+    // For a program of length 16, and output of the same size,
+    // we need exactly 16 loops, so A must contains non-zero bits
+    // in its bits located at 16*3[0,+1,+2] (or 15*3 ?)and none above.
+
+    // This leads to a range of 35184372088832..
+    //                          70368744177664,
+    // not really tractable either.
+    // Actual result            107416732707226 so there was one magnitude error...
+
+    
+    if false {
+        brute_force(&mut machine);
+    }
+
+    // Obviously A itself contains some encoded version of the program
+    // decoded octal by octal. Xor and dynamic shifts don't make it
+    // easy to reverse-engineer; ideally we want to construct A by running the
+    // program in reverse starting from the expected output.
+    // However, the xor are limited, by the literal operand, to 0..7,
+    // and code dissassembly show that indirect shifts (by ComboOperand)
+    // use registers that are themselves assigned by BST (3 bits max) and xored
+    // with other literals. So total range of "cascading" bits from A to the final
+    // output value does not grow above something like 8 bits.
+
+    /*
+    ============
+    00: BST A
+    02: BXL 5
+    04: CDV B
+    06: BXL 6
+    08: BXC
+    10: OUT B % 8
+    12: ADV 3
+    14: JNZ 0
+    ============
+    00: B = A % 8
+    02: B = B xor 5
+    04: C = A >> B
+    06: B = B xor 6
+    08: B = B xor C
+    10: OUT B % 8
+    12: A = A >> 3
+    14: If A != 0 JMP 0
+    ============
+     */
+
+    // Therefore, each generated output is only depending on a limited range of lower bits
+    // from A at each step (A being shifted each time).
+    // We can chunk the search output number by output number instead of all at once:
+    // Once the first valid out number (equals to the first program number) has found all its possible
+    // A register values generating it, from "slightly brute-forcing" only on a range of 0..256,
+    // the second out number will be generated from a slightly
+    // modified A, shifted by 3 bits and a few xors.
+
+    let mut valid_a = Vec::<usize>::new();
+
+    //valid_a.push(0); // Just to avoid special_casing the first digit
+    valid_a.push(42567035290); // This seed is the decimal version of the truncated octal value common to all results
+                               // ( 0x42567035290) found by initial algorithm but which were "too high", generating all 14 first digits.
+                               // This leads to the correct result 107416732707226 (oct 0x3033075014424632) instead
+                               // of the one found by first algo,  107416748386714 (oct 0x3033075110264632)
+
+    // Still searching why the standard starting point overshoots
+
+    // loop search takes 7s from starting at 0, and 20s starting from the "magic" seed.
+    
+    let mut range_factor = 1;
+    let bits = 8;
+
+    for digit in 1..=machine.program.len() {
+        let mut next_valid_a = Vec::<usize>::new();
+
+        for prev in &valid_a {
+            // "256" was too low (no matching digit after 7), 512 is goodenough.
+            for k in 0..512 {
+                let a = *prev + k * range_factor;
+                machine.reset_with_register(a);
+                let quine = if digit == machine.program.len() {
+                    // For final loop we require exact match, not a prefix
+                    // that continues for longer.
+                    machine.run_until_halt_or_non_quine()
+                } else {
+                    machine.run_until_halt_or_non_quine_or_outlen(digit)
+                };
+                if quine {
+                    machine.print_output();
+                    println!("Partial found : First {digit} matching characters found for A = {a}");
+                    next_valid_a.push(a);
+                }
+            }
+        }
+
+        if next_valid_a.len() == 0 {
+            panic!(
+                "No candidate A found to match the first {digit} output ! Must expand search range"
+            );
+        }
+
+        println!("Previous lowest A was {}", valid_a[0]);
+
+        // Collect our different candidates for next digit.
+        // Since we have overlaps (k covers more than just the new bits factor),
+        // need to deduplicate first. Also it will sort for final result.
+        next_valid_a.sort();
+        next_valid_a.dedup();
+        valid_a = next_valid_a;
+
+        range_factor *= bits;
+    }
+
+    println!("All valid_A = {:?}", valid_a);
+    valid_a.truncate(10);
+
+    println!("Part 2: Valid A for complete quine ?");
+    for a in &valid_a {
+        machine.reset_with_register(*a);
+        if machine.run_until_halt_or_non_quine() {
+            machine.print_output();
+            println!("Found register A = {} == oct {:o}", *a, *a);
+        } else {
+            machine.print_output();
+            println!("register A = {} is invalid !! (bug)", *a);
+        }
+    }
+
+    /* debug */
+    let h = 42567035290;
+    machine.reset_with_register(h);
+    if machine.run_until_halt_or_non_quine() {
+        machine.print_output();
+        println!("Found hardcoded register A = {h}",);
+    } else {
+        println!("hardcoded {h} is not quine:",);
+        machine.print_output();
+    }
+
+    println!("Part 2 : First valid A is {}", valid_a[0]);
 }
